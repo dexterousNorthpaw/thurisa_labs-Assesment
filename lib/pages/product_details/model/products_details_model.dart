0dbe4187e4b8a958c2c12afaed0869b68b027145
Rayhan Awojobi <dexterousnorthpaw@gmail.com>
2023-07-05 15:24:38 +0100

    created product details model

---
+++ lib/pages/product_details/model/products_details_model.dart
@@ -0,0 +1,69 @@
+// To parse this JSON data, do
+//
+//     final productDetailsModel = productDetailsModelFromJson(jsonString);
+
+import 'dart:convert';
+
+ProductDetailsModel productDetailsModelFromJson(String str) => ProductDetailsModel.fromJson(json.decode(str));
+
+String productDetailsModelToJson(ProductDetailsModel data) => json.encode(data.toJson());
+
+class ProductDetailsModel {
+  int id;
+  String title;
+  double price;
+  String description;
+  String category;
+  String image;
+  Rating rating;
+
+  ProductDetailsModel({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.image,
+    required this.rating,
+  });
+
+  factory ProductDetailsModel.fromJson(Map<String, dynamic> json) => ProductDetailsModel(
+    id: json["id"],
+    title: json["title"],
+    price: json["price"],
+    description: json["description"],
+    category: json["category"],
+    image: json["image"],
+    rating: Rating.fromJson(json["rating"]),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "id": id,
+    "title": title,
+    "price": price,
+    "description": description,
+    "category": category,
+    "image": image,
+    "rating": rating.toJson(),
+  };
+}
+
+class Rating {
+  double rate;
+  int count;
+
+  Rating({
+    required this.rate,
+    required this.count,
+  });
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+    rate: json["rate"]?.toDouble(),
+    count: json["count"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "rate": rate,
+    "count": count,
+  };
+}
