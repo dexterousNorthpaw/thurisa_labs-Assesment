92c7413b37594521da8c9fb05b90daf9063c7138
Rayhan Awojobi <dexterousnorthpaw@gmail.com>
2023-07-05 15:30:41 +0100

    fix

--- lib/pages/drawer_screen.dart
+++ lib/pages/drawer_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
 import 'package:thurisa_labs/pages/home/ui/home_page.dart';
 import 'package:thurisa_labs/pages/menu_screen.dart';
@@ -16,18 +17,23 @@ class _DrawerScreenState extends State<DrawerScreen> {
   final zoomDrawController = ZoomDrawerController();
   @override
   Widget build(BuildContext context) {
-    return ZoomDrawer(
-      showShadow: true,
-        drawerShadowsBackgroundColor: Colors.orange,
-      menuBackgroundColor: Color.fromRGBO(250, 74, 12, 1),
-      // drawerShadowsBackgroundColor: Colors.white10,
-      mainScreenTapClose: true,
-      menuScreenTapClose: true,
-      androidCloseOnBackTap: true,
-      angle: 0,
-      controller: zoomDrawController,
-        menuScreen: MenuScreen(),
-        mainScreen: HomePage()
-    );
+    return ScreenUtilInit(
+        designSize: const Size(414, 896),
+        builder: (BuildContext context,child){
+
+      return ZoomDrawer(
+          showShadow: true,
+          drawerShadowsBackgroundColor: Colors.orange,
+          menuBackgroundColor: Color.fromRGBO(250, 74, 12, 1),
+          // drawerShadowsBackgroundColor: Colors.white10,
+          mainScreenTapClose: true,
+          menuScreenTapClose: true,
+          androidCloseOnBackTap: true,
+          angle: 0,
+          controller: zoomDrawController,
+          menuScreen: MenuScreen(),
+          mainScreen: HomePage()
+      );
+    });
   }
 }
